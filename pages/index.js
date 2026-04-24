e03ce86705cd2306f8f55de8e5eb384d6d91d1a5
Willian Augusto <62366661+willian16041994@users.noreply.github.com>
2026-04-24 11:25:59 +0000

    identando o código

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,25 @@
 function Home() {
-    return <div style={{ display: "flex", height: "100vh", alignItems: "center", justifyContent: "center", background: "linear-gradient(135deg, #1e1e2f, #2a2a40)", color: "#fff", fontFamily: "Arial, sans-serif", textAlign: "center", padding: "20px" }}> <h1 style={{ maxWidth: "700px", lineHeight: "1.6" }}> Fiz essa página pensando em você ❤️ <br /><br /> Tenha uma ótima sexta-feira! <br /> Espero que nossa caminhada juntos seja repleta de momentos felizes e que, diante de qualquer obstáculo, possamos sempre enfrentá-los lado a lado ❤️ </h1> </div>
+    return <div style={
+        {
+            display: "flex",
+            height: "100vh",
+            alignItems: "center",
+            justifyContent: "center",
+            background: "linear-gradient(135deg, #1e1e2f, #2a2a40)",
+            color: "#fff",
+            fontFamily: "Arial, sans-serif", textAlign: "center",
+            padding: "20px"
+        }}>
+        <h1 style={
+            {
+                maxWidth: "700px",
+                lineHeight: "1.6"
+            }}>
+            Fiz essa página pensando em você ❤️ <br /><br />
+            Tenha uma ótima sexta-feira! <br />
+            Espero que nossa caminhada juntos seja repleta de momentos felizes e que, diante de qualquer obstáculo, possamos sempre enfrentá-los lado a lado ❤️<br />
+            Ass. Willian
+        </h1>
+    </div>
 }
 export default Home;

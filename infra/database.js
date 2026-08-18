4b9a35f8b76eb2beb3a9a8f5adc7afc2325f6066
Willian Augusto <62366661+willian16041994@users.noreply.github.com>
2026-08-18 12:26:40 +0000

    feat: add variaveis de ambiente

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,11 @@ import { Client } from "pg";
 
 async function query(query) {
   const client = new Client({
-    host: process.env.HOST,
-    port: process.env.PORT,
-    user: process.env.USER,
-    database: "postgres",
-    password: "local_password",
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
   });
 
   await client.connect();
